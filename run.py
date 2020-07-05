5a3e304fbf8e8af7859425950f09cba8269d3ac5
Kartik-Sangwan <kartiksangwan29@gmail.com>
2020-07-05 13:54:54 -0400

    debug

--- run.py
+++ run.py
@@ -2,5 +2,5 @@ from main import app, db
 import os
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 5000))
-	app.run(host = '0.0.0.0', port=port)
+	app.run(host = '0.0.0.0', port=port, debug=True)
 	# db.create_all()
